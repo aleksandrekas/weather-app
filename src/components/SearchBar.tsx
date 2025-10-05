25c66ad08d40d48898f1e5f77b3a8c480fcba200
aleksandre <aleksandrekasrashvili@gmail.com>
2025-10-05 15:39:58 +0400

    added search function for cities fully showing results

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import searchIcon from "../assets/icons/Search Icon.svg"
-import { useEffect, useState } from "react"
 import loading from "../assets/icons/loading.svg"
+import { useEffect, useState } from "react"
 import CityBar from "./CityBar"
 type CityType = {
     name : string;
@@ -11,41 +11,24 @@ type CityType = {
 
 
 export default function SearchBar(){
-    const [city,setCity] = useState({
-        cityname:'',
-        searchName:''
-    })
+    const [city,setCity] = useState('')
     const [cityNames,setCityNames] = useState<CityType[]>([])
+    const [focus,setFocus] = useState<Boolean>(false)
     
 
     function handleInputChange(e:React.ChangeEvent<HTMLInputElement>){
-        setCity((prev)=>({
-            ...prev,
-            cityname:e.target.value
-        }))
+        setCity(e.target.value)
+        setFocus(true)
     }
 
 
-    useEffect(()=>{
-        const searchNameHandler = setTimeout(()=>{
-            // setting 1 sec delay for seachName,purpose:
-            // npminatim api allows only 1 request per second for city names 
-            setCity((prev)=>({
-                ...prev,
-                searchName:city.cityname
-            }))
-        },1000)
 
-        return ()=>{
-            clearTimeout(searchNameHandler)
-        }
-    },[city.cityname])
 
     useEffect(()=>{
         (async()=>{
-            if(city.searchName != ""){
+            if(city != ""){
                 try{
-                    const cities = await fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(city.searchName)}&limit=5&apiKey=2e95952892144e7397ac3df304bce5a2`,{
+                    const cities = await fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(city)}&limit=5&apiKey=2e95952892144e7397ac3df304bce5a2`,{
                         headers:{
                             "Content-Type":"application/json"
                         }
@@ -53,19 +36,27 @@ export default function SearchBar(){
     
                     const data = await cities.json()
     
-                    data.features.map((item)=>{
-                        setCityNames((prev)=>[
-                            {name:item.properties.formatted,lat:item.properties.lat,long:item.properties.lon},
-                            ...prev
-                        ])
-                    })
+                    setCityNames(
+                        data.features.map((item: any) => ({
+                        name: item.properties.formatted,
+                        lat: item.properties.lat,
+                        long: item.properties.lon,
+                        }))
+                    );
     
                 }catch(err){
                     console.error(err)
                 }
+            }else{
+                    setCityNames([])
+                    setFocus(false)
             }
         })()
-    },[city.searchName])
+
+
+
+ 
+    },[city])
 
 
     console.log(cityNames)
@@ -75,16 +66,19 @@ export default function SearchBar(){
         <div className="searchBar">
             <h1>How's the sky looking today?</h1>
             <form className="searchForm">
-                <input onChange={handleInputChange} type="text" placeholder="Search for a place..." />
-                <input type="submit" value="Search" />
-                <img src={searchIcon} alt="" />
-                <div className="searchResultContainer" style={{display: city.cityname === ''? 'none' : 'block'}}>
-                    {
-                        cityNames.length === 0 ?
-                        <div className="progress">
-                        </div> :
-                        
-                    }
+                <input onBlur={()=>{setFocus(false)}} className="searchFormInput" onChange={handleInputChange} type="text" placeholder="Search for a place..." />
+                <input  className="submitBtn" type="submit" value="Search" />
+                <img className="searchIcon" src={searchIcon} alt="" />
+                <div className="searchResultContainer" style={{display: !focus ? 'none' : 'flex'}}>
+                    {cityNames.length === 0 ?
+                        <div className="searching">
+                            <img src={loading} alt="loading" />
+                            <h4>Searching</h4>
+                        </div>
+                    :
+                        cityNames.map((item,index)=>(
+                            <CityBar key={index} name={item.name} lat={item.lat} lon={item.long} />
+                        ))}
                 </div>
             </form>
         </div>
